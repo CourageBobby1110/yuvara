c6726e1e9539f1fabdca3233ea8901ea6aefe2be
Chidi Courage Bobby <ravensraven80@gmail.com>
2026-03-19 15:40:36 +0100

    updated

--- yuvara-mobile/capacitor.config.ts
+++ yuvara-mobile/capacitor.config.ts
@@ -1,13 +1,23 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.yuvara.app',
-  appName: 'yuvara',
-  webDir: 'www',
+  appId: "com.yuvara.app",
+  appName: "yuvara",
+  webDir: "www",
   server: {
-    url: 'https://yuvara.netlify.app',
-    cleartext: true
-  }
+    url: "https://yuvara.netlify.app",
+    cleartext: true,
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#000000",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+    },
+  },
 };
 
 export default config;
